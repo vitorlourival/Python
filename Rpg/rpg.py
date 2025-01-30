82702d1d724b6bdaa970d62e5ac40a5c831893d6
vitorLourival22 <vitorLou2204@gmail.com>
2025-01-29 21:41:41 -0300

    rpg

--- Rpg/rpg.py
+++ Rpg/rpg.py
@@ -17,16 +17,20 @@ monstros = [
     "alien"
 ]
 
-enemy = random.choice(monstros)
 player = input("Digite Seu Nome: ")
 print(f"Bem Vindo(a) {player}")
+enemy = random.choice(monstros)
+print(f"Seu Oponente {enemy}")
 print(f"Turno {turnos}")
-while True:
-    print(f"Seu Oponente {enemy}")
+while life and life_enemy > 0:
+    print(f"Turno: {turnos}")
+    print(f"Player Life: {life}  Enemy Life: {life_enemy}")
+
     for atk in habilidades:
-        atkEnm = random.choice(habilidades)
+        #atkEnm = random.choice(habilidades)
         print(f"Life {life}")
         print("Corte,Flecha,Magia,defesa,cura")
+
         atk = input("Selecione um Ataque: ")
         if atk == "corte" or "flecha" or "magia":
           if enemy == "defesa":
@@ -34,6 +38,22 @@ while True:
           else:
              life_enemy -10
              print(f"{enemy} Sofreu 10 de dano")
+        elif atk == "defesa":
+           print(f"Player usou {atk}")
+    
+    for atkEnm in habilidades:
+        atkEnm = random.choice(habilidades)
+        print(f"Life {life}")
+        print("Corte,Flecha,Magia,defesa,cura")
+
+        if atkEnm == "corte" or "flecha" or "magia":
+          if player == "defesa":
+             print(f"{player} Defendeu Seu Ataque")
+          else:
+             life -10
+             print(f"{player} Sofreu 10 de dano")
+        elif atkEnm == "defesa":
+           print(f"Player usou {atk}")
     
     
     
